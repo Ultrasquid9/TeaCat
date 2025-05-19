c733e6643b12664eff33f4eb948e89e2a44c719b
Ultrasquid9 <ultrasquid.epic@gmail.com>
2025-05-19 18:52:55 -0400

    Rewrite Lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,46 +1,31 @@
-use std::mem::replace;
+use nom::error::Error as NomError;
 
-pub type TokenTree = Vec<Token>;
+use nom::bytes::complete::tag;
 
-#[derive(Debug, Clone)]
+pub type TokenStream = Vec<Token>;
+
+#[derive(PartialEq, Eq, Debug)]
 pub enum Token {
-	/// No element present (ignore when parsing)
-	None,
-	/// A simple text element
+	Ident(String),
 	Text(String),
 
-	/// Defining an HTML tag
-	Tag(String),
-	/// The start of an HTML tag
-	StartTag,
-	/// The end of an HTML tag
-	EndTag,
+	OpenBracket,
+	CloseBracket,
+	OpenBrace,
+	CloseBrace,
 
-	/// Defining a tag attribute
-	Attribute(String),
-	/// The start of a list of attributes
-	StartAttribute,
-	/// The end of a list of attributes
-	EndAttribute,
+	Walrus,
+	Colon,
+	SemiColon,
+	Andpersand,
 
-	/// Defining a variable with the given name
-	Var(String),
-	/// The start of the contents of a variable
-	StartVar,
-	/// The end of the contents of a variable
-	EndVar,
-	/// Accessing a variable with the given name  
-	AccessVar(String),
+	None,
 }
 
 impl Token {
 	fn string_mut(&mut self) -> Option<&mut String> {
 		Some(match self {
-			Self::Tag(str)
-			| Self::Text(str)
-			| Self::Attribute(str)
-			| Self::Var(str)
-			| Self::AccessVar(str) => str,
+			Self::Text(str) | Self::Ident(str) => str,
 
 			_ => return None,
 		})
@@ -52,83 +37,108 @@ impl Token {
 		}
 	}
 
-	fn tag_allowed(&self) -> bool {
-		!matches!(
-			self,
-			Self::Tag(_) | Self::Attribute(_) | Self::StartAttribute | Self::EndAttribute
-		)
+	fn creates_ident(&self) -> bool {
+		matches!(self, Self::Colon | Self::Andpersand)
 	}
 }
 
-pub fn lex(input: String) -> TokenTree {
-	let mut tokens = vec![];
+pub fn lex(mut input: String) -> TokenStream {
+	let mut tokenstream = vec![];
 	let mut current = Token::None;
 
 	macro_rules! token_switcheroo {
 		($new:expr) => {{
-			let token = replace(&mut current, $new);
-			tokens.push(token);
+			let token = std::mem::replace(&mut current, $new);
+			tokenstream.push(token);
 		}};
 	}
 
-	let mut is_comment = false;
-	let mut is_var = false;
-	let mut is_escape = false;
+	let mut escaped = false;
 
-	for ch in input.chars() {
-		if is_escape && !is_comment {
-			current.push_char(ch); // TODO: Custom handling of newlines & such 
-			is_escape = false;
-			continue;
-		}
-		if is_comment {
-			if ch == '\n' {
-				is_comment = false;
+	while input.len() > 0 {
+		// Handling comments
+		if str_starts_with(&input, "#") {
+			if let Some((_, str)) = input.split_once("\n") {
+				input = str.into();
+			} else {
+				input.clear();
 			}
 			continue;
 		}
 
-		match ch {
-			'#' => is_comment = true,
-			'\\' => is_escape = true,
-			':' if current.tag_allowed() => token_switcheroo!(Token::Tag("".into())),
+		// Handling the backslash escape
+		// TODO: \n, \t, etc
+		if escaped {
+			current.push_char(input.remove(0));
+			escaped = false;
+			continue;
+		}
+		if str_starts_with(&input, "\\") {
+			escaped = true;
+			continue;
+		}
 
-			'[' => {
-				token_switcheroo!(Token::StartTag);
-				token_switcheroo!(Token::Text("".into()));
-			}
-			']' => token_switcheroo!(Token::EndTag),
+		// Checks for one of the operators/keywords is present
+		if let Some(token) = rules(&mut input) {
+			token_switcheroo!(token);
+			continue;
+		}
 
-			'{' => {
-				token_switcheroo!(Token::StartAttribute);
-				token_switcheroo!(Token::Attribute("".into()))
-			}
-			'}' => token_switcheroo!(Token::EndAttribute),
-			',' if matches!(current, Token::Attribute(_)) => {
-				token_switcheroo!(Token::Attribute("".into()))
-			}
+		// No operators/keywords found, so the current char is inserted into the current token
+		let ch = input.remove(0);
 
-			'$' => {
-				is_var = true;
-				token_switcheroo!(Token::Var("".into()))
-			}
-			whitespace if whitespace.is_whitespace() && matches!(current, Token::Var(_)) => {
-				token_switcheroo!(Token::StartVar);
-				token_switcheroo!(Token::Text("".into()))
-			}
-			whitespace if whitespace.is_whitespace() && matches!(current, Token::AccessVar(_)) => {
-				token_switcheroo!(Token::Text("".into()))
-			}
-			';' if is_var => {
-				is_var = false;
-				token_switcheroo!(Token::EndVar);
-			}
-			'&' => token_switcheroo!(Token::AccessVar("".into())),
+		// If the current Token does not store text, set it to one that does.
+		// - If the current Token implies an Ident, create one.
+		// - Otherwise, create a Text.
+		if current.string_mut().is_none() {
+			let token = if current.creates_ident() {
+				Token::Ident("".into())
+			} else {
+				Token::Text("".into())
+			};
+			token_switcheroo!(token);
+		}
+		// An Ident cannot contain whitespace.
+		if matches!(current, Token::Ident(_)) && ch.is_whitespace() {
+			token_switcheroo!(Token::Text("".into()))
+		}
 
-			other => current.push_char(other),
+		current.push_char(ch);
+	}
+
+	tokenstream.push(current);
+	tokenstream
+}
+
+fn rules(input: &mut String) -> Option<Token> {
+	let rules = [
+		("[", Token::OpenBracket),
+		("]", Token::CloseBracket),
+		("{", Token::OpenBrace),
+		("}", Token::CloseBrace),
+		(":=", Token::Walrus),
+		(":", Token::Colon),
+		(";", Token::SemiColon),
+		("&", Token::Andpersand),
+	];
+
+	for (key, token) in rules {
+		if tag::<&str, &str, NomError<_>>(key)(input).is_ok() {
+			*input = input.replacen(key, "", 1);
+			return Some(token);
 		}
 	}
 
-	tokens.push(current);
-	tokens
+	None
+}
+
+fn str_starts_with(input: &str, pat: &str) -> bool {
+	tag::<&str, &str, NomError<_>>(pat)(input).is_ok()
+}
+
+#[test]
+fn test() {
+	assert_eq!(rules(&mut "nothing".into()), None);
+	assert_eq!(rules(&mut ":=".into()), Some(Token::Walrus));
+	assert_eq!(rules(&mut "ab := cd".into()), None);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,15 @@
-use eval::eval;
-use expansion::variables;
-use lexer::lex;
+use std::error::Error;
 
-pub mod eval;
-pub mod expansion;
 pub mod lexer;
 
 const INPUT: &str = r#"
 # Comments use hashtags
 
 # Variables
-$hello_world Hello, World!;
+&hello_world := Hello, World!;
 
 # Just about anything can be assigned to a variable
-$title :title[
+&title := :title[
 	My Webpage
 ];
 
@@ -42,11 +38,20 @@ $title :title[
 ]
 "#;
 
-fn main() {
-	let tokens = lex(INPUT.into());
+pub type CatResult<T> = Result<T, Box<dyn Error + Send + Sync>>;
+
+fn main() -> CatResult<()> {
+	let tokenstream = lexer::lex(INPUT.into());
+	for token in tokenstream {
+		println!("{token:?}")
+	}
+
+/* 	let tokens = lex(INPUT.into());
 	let tokens = variables(tokens);
 	let html = eval(tokens);
 
 	println!("{}", html);
-	_ = std::fs::write("./index.html", html);
+	std::fs::write("./index.html", html)?; */
+
+	Ok(())
 }
